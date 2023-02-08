0335b81af5f414f00fd1e8edf333ab1f9bb9de18
misterjacobo97 <mister.jacobo97@gmail.com>
2023-02-08 22:01:11 +1100

    added a more dynamic way to select icons

--- Javascript/cafe_info.js
+++ Javascript/cafe_info.js
@@ -1,22 +1,22 @@
 // an array of coordinates for CHAI ME
 var coordinatesChaiMe = [
-    { lat: -33.882948709789, lng: 151.1914159115681, title: "Sapphos", Soy: "", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "Yes" },
-    { lat: -33.887869824610696, lng: 151.21438351549025, title: "Bourke St Surry Hills", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "Yes" },
-    { lat: -33.86435381387368, lng: 151.20142669807498, title: "Bourke St Barangaroo", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "Yes" }
+    { lat: -33.882948709789, lng: 151.1914159115681, title: "Sapphos", chai: "Chai Me!", Soy: "", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "Yes" },
+    { lat: -33.887869824610696, lng: 151.21438351549025, title: "Bourke St Surry Hills", chai: "Chai Me!", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "Yes" },
+    { lat: -33.86435381387368, lng: 151.20142669807498, title: "Bourke St Barangaroo", chai: "Chai Me!", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "Yes" }
 ];
 
 
 // an array of coordinates for VEGAN CHAI
 var coordinatesVeganOptions = [
-    { lat: -33.88226770573398, lng: 151.19015070768268, title: "Oh My Days", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "No" },
-    { lat: -33.88360595953672, lng: 151.19162860482174, title: "Lilypad Sydney", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "" },
-    { lat: -33.868925740936255, lng: 151.20771357624943, title: "Romolo Pitt St", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "" },
-    { lat: -33.86802294087114, lng: 151.20623359868333, title: "Three Beans York St", Soy: "Bonsoy", Oat: "", Almond: "", Date: "Jan 2023", Extra: "Yes" },
-    { lat: -33.883664249143415, lng: 151.20961248317948, title: "Shift Eatery", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "No" },
-    { lat: -33.79425745152499, lng: 151.1860205554046, title: "Espresso Zarcino ", Soy: "Bonsoy", Oat: "", Almond: "", Date: "Jan 2023", Extra: "Yes" }
+    { lat: -33.88226770573398, lng: 151.19015070768268, title: "Oh My Days", chai: "Vegan chai", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "No" },
+    { lat: -33.88360595953672, lng: 151.19162860482174, title: "Lilypad Sydney", chai: "Vegan chai", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "" },
+    { lat: -33.868925740936255, lng: 151.20771357624943, title: "Romolo Pitt St", chai: "Vegan chai", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "" },
+    { lat: -33.86802294087114, lng: 151.20623359868333, title: "Three Beans York St", chai: "Vegan chai", Soy: "Bonsoy", Oat: "", Almond: "", Date: "Jan 2023", Extra: "Yes" },
+    { lat: -33.883664249143415, lng: 151.20961248317948, title: "Shift Eatery", chai: "Vegan chai", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "No" },
+    { lat: -33.79425745152499, lng: 151.1860205554046, title: "Espresso Zarcino ", chai: "Vegan chai", Soy: "Bonsoy", Oat: "", Almond: "", Date: "Jan 2023", Extra: "Yes" }
 ];
 
 // an array of coordiantes for NOT ETHICAL 
 var coordinatesNotEthical = [
-    { lat: -32.88226770573398, lng: 151.19015070768268, title: "PlaceHolder", Soy: "", Oat: "", Almond: "", Date: "Nov 2022" },
+    { lat: -32.88226770573398, lng: 151.19015070768268, title: "PlaceHolder", chai: "Nope.", Soy: "", Oat: "", Almond: "", Date: "Nov 2022" },
 ];
