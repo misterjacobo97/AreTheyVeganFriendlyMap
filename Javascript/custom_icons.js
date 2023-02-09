8ed40c6794a0b5c4a959d11d58614c290f401e7b
Jacob <mister.jacobo97@gmail.com>
2023-02-09 15:55:20 +1100

    added cafes and changed the icon colours

--- Javascript/custom_icons.js
+++ Javascript/custom_icons.js
@@ -1,20 +1,20 @@
 // custom Icons
 var GreenIcon = L.icon({
-    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(86,113,90)"></path></svg>',
+    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(163,226,173)"></path></svg>',
     iconSize: [30, 30],
     iconAnchor: [12.5, 41],
     popupAnchor: [0, -41]
 })
 
 var RedIcon = L.icon({
-    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(214,50,50)"></path></svg>',
+    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(255,174,66)"></path></svg>',
     iconSize: [30, 30],
     iconAnchor: [12.5, 41],
     popupAnchor: [0, -41]
 })
 
-var YellowIcon = L.icon({
-    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(241,194,50)"></path></svg>',
+var BlueIcon = L.icon({
+    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(123,182,193)"></path></svg>',
     iconSize: [30, 30],
     iconAnchor: [12.5, 41],
     popupAnchor: [0, -41]
@@ -22,12 +22,12 @@ var YellowIcon = L.icon({
 
 
 function custom_icon(vegan, extra){
-
+// returns an icon colour depending if the cafes are fully vegan or have vegan chai or not
     if(vegan == "Yes"){
         return GreenIcon;
     }
     else if(vegan == "No" && extra == "Yes"){
-        return YellowIcon;
+        return BlueIcon;
     }
     else if(vegan == "No" && extra != "Yes"){
         return RedIcon;
