6992e419eb5a98e845a6976e9a64a3f9422a439d
Jacob <mister.jacobo97@gmail.com>
2023-02-09 14:43:39 +1100

    added a more efficient and dynamic way to load in cafe objects

---
+++ Javascript/layergroup_functions.js
@@ -0,0 +1,12 @@
+// function to send makers to the appropriate layer group
+function select_layer_group(cafe){
+    if(cafe == "Chai Me!"){
+        return markerChaiMeGroup;
+    }
+    else if(cafe == "Vegan chai"){
+        return markerVeganGroup;
+    }
+    else if(cafe == "nope."){
+        return markerNotEthicalGroup;
+    }
+}
