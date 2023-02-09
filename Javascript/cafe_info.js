466d88a5d4d6b22ae493780a3db8838a7e5525ad
Jacob <mister.jacobo97@gmail.com>
2023-02-10 09:24:48 +1100

    changed the svg code for the icons

--- Javascript/cafe_info.js
+++ Javascript/cafe_info.js
@@ -23,10 +23,10 @@
 
 // an array of cafe objects
 var coordinatesX = [
-    //vegan
+//vegan
     { lat: -33.88226770573398, lng: 151.19015070768268, title: "Oh My Days", vegan: "Yes", chai: "Vegan chai", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "No" },
     { lat: -33.883664249143415, lng: 151.20961248317948, title: "Shift Eatery", vegan: "Yes", chai: "Vegan chai", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "No" },
-    //non-vegan
+//non-vegan
     { lat: -33.882948709789, lng: 151.1914159115681, title: "Sapphos", vegan: "No", chai: "Chai Me!", Soy: "", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "Yes" },
     { lat: -33.87102485260521,  lng: 151.20674652513898, title: "Underground Espresso", vegan: "No", chai: "Chai Me!", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "Milklab", Date: "Feb 2023", Extra: "Yes" },
     { lat: -33.887869824610696, lng: 151.21438351549025, title: "Bourke St Surry Hills", vegan: "No", chai: "Chai Me!", Soy: "Bonsoy", Oat: "Alt Dairy Co", Almond: "", Date: "Nov 2022", Extra: "Yes" },
@@ -37,4 +37,5 @@ var coordinatesX = [
     { lat: -33.86517145711799, lng: 151.20287178783363, title: "Wild Sage Barangaroo", vegan: "No", chai: "Nope.", Soy: "", Oat: "Milklab", Almond: "", Date: "Sep 2022", Extra: "Yes" },
     { lat: -33.81387874694072, lng: 151.1694311304095, title: "Birdwood Cafe", vegan: "No", chai: "Vegan chai", Soy: "Bonsoy", Oat: "", Almond: "", Date: "Nov 2022", Extra: "Yes" },
     { lat: -33.86435381387368, lng: 151.20142669807498, title: "Bourke St Barangaroo", vegan: "No", chai: "Chai Me!", Soy: "", Oat: "", Almond: "", Date: "Nov 2022", Extra: "Yes" }
+//non-cafe vegan places
 ];
--- Javascript/custom_icons.js
+++ Javascript/custom_icons.js
@@ -1,20 +1,20 @@
 // custom Icons
 var GreenIcon = L.icon({
-    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(163,226,173)"></path></svg>',
+    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 388 515"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(163,226,173)" stroke="black" stroke-width="3"></path></svg>',
     iconSize: [30, 30],
     iconAnchor: [12.5, 41],
     popupAnchor: [0, -41]
 })
 
 var RedIcon = L.icon({
-    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(255,174,66)"></path></svg>',
+    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 388 515"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(255,174,66)" stroke="black" stroke-width="3"></path></svg>',
     iconSize: [30, 30],
     iconAnchor: [12.5, 41],
     popupAnchor: [0, -41]
 })
 
 var BlueIcon = L.icon({
-    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(123,182,193)"></path></svg>',
+    iconUrl: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 388 515"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z" fill="rgb(123,182,193)" stroke="black" stroke-width="3"></path></svg>',
     iconSize: [30, 30],
     iconAnchor: [12.5, 41],
     popupAnchor: [0, -41]
